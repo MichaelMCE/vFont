3aaac71de736b19ec3a2e3a38b19408b9c50e0f8
MichaelMCE <mapei_@hotmail.com>
2019-11-19 15:42:47 +0000

    Indicate libvfont.cpp is for building Arduino on only

--- libvfont.cpp
+++ libvfont.cpp
@@ -21,7 +21,11 @@
 //	License along with this library; if not, write to the Free
 //	Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
+/*
 
+Arduino only
+
+*/
 
 #include "vfont/vfont.cpp"
 #include "vfont/primitives.cpp"
